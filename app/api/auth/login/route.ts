185fbfe664b59daa6f8b981be1312d331c449573
Dody Zulfan <47063086+Doduyxuvan@users.noreply.github.com>
2026-05-10 06:04:55 +0700

    Update route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import bcrypt from 'bcryptjs'
-import { supabase } from '@/lib/db'
+import { supabase } from '@/lib/supabaseServer'
 import { signToken } from '@/lib/auth'
 
 export async function POST(req: NextRequest) {
